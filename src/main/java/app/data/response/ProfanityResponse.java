f764c489203e4bd04213c7523609737d3a36bb81
whale0928 <rlagusrl928@gmail.com>
2024-06-18 03:55:44 +0900

    feat: 응답 객체 수정

--- src/main/java/app/data/response/ProfanityResponse.java
+++ src/main/java/app/data/response/ProfanityResponse.java
@@ -10,12 +10,17 @@ public record ProfanityResponse(
 
     private final static String SUCCESS_MESSAGE = "비속어가 존재합니다.";
     private final static String NO_PROFANITY_MESSAGE = "비속어가 존재하지 않습니다.";
+    private final static String ERROR_MESSAGE = "비속어 검사에 실패했습니다.";
 
     public static ProfanityResponse success(Boolean isProfane) {
-        return new ProfanityResponse(HttpStatus.OK, SUCCESS_MESSAGE, isProfane);
+        return new ProfanityResponse(
+                HttpStatus.OK,
+                isProfane ? SUCCESS_MESSAGE : NO_PROFANITY_MESSAGE,
+                isProfane
+        );
     }
 
     public static ProfanityResponse fail(Boolean isProfane) {
-        return new ProfanityResponse(HttpStatus.BAD_REQUEST, NO_PROFANITY_MESSAGE, isProfane);
+        return new ProfanityResponse(HttpStatus.BAD_REQUEST, ERROR_MESSAGE, isProfane);
     }
 }
