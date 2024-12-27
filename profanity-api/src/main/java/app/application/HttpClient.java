1ff2cd8e0021a248fa02dc379c36a323ffc78b5f
hgkim <hgkim@openrd.com>
2024-12-28 02:17:28 +0900

    refactor: add NoArgConstructor at client utils

--- profanity-api/src/main/java/app/application/HttpClient.java
+++ profanity-api/src/main/java/app/application/HttpClient.java
@@ -1,6 +1,8 @@
 package app.application;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.Objects;
 
 @Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HttpClient {
 
     private static final Logger log = LogManager.getLogger(HttpClient.class);
