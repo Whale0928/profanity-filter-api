2810beb7226490bb5949abe313e7be1b6ecafdda
Living whale <rlagusrl928@gmail.com>
2024-12-30 00:47:37 +0900

    Pr/20 remove special characters (#21)
    Co-authored-by: hgkim <hgkim@openrd.com>

--- profanity-domain/src/test/java/app/application/filter/DefaultProfanityHandlerTest.java
+++ profanity-domain/src/test/java/app/application/filter/DefaultProfanityHandlerTest.java
@@ -19,22 +19,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class DefaultProfanityHandlerTest {
 
     private static final Logger log = LogManager.getLogger(DefaultProfanityHandlerTest.class);
-    private ProfanityHandler ProfanityHandler;
+    private ProfanityHandler profanityHandler;
 
     @BeforeEach
     void setUp() {
         InmemoryProfanityRepository repository = new InmemoryProfanityRepository();
-        QuickProfanityFilter quickProfanityFilter = new QuickProfanityFilter();
         NormalProfanityFilter normalProfanityFilter = new NormalProfanityFilter(repository);
-        AdvancedProfanityFilter advancedProfanityFilter = new AdvancedProfanityFilter();
         FakeApplicationEventPublisher eventPublisher = new FakeApplicationEventPublisher();
 
         repository.save(ProfanityWord.create("욕설"));
         repository.save(ProfanityWord.create("나쁜놈"));
         repository.save(ProfanityWord.create("비속어"));
+        repository.save(ProfanityWord.create("씨뻘"));
         normalProfanityFilter.synchronizeProfanityTrie();
 
-        ProfanityHandler = new DefaultProfanityHandler(quickProfanityFilter, normalProfanityFilter, advancedProfanityFilter, eventPublisher);
+        profanityHandler = new DefaultProfanityHandler(normalProfanityFilter, eventPublisher);
     }
 
 
@@ -45,7 +44,7 @@ class DefaultProfanityHandlerTest {
         String word = "'씨뻘'욕설이 들어간 문장입니다.";
 
         // when
-        ApiResponse response = ProfanityHandler.quickFilter(word);
+        ApiResponse response = profanityHandler.quickFilter(word);
 
         // then
         Set<Detected> detected = response.detected();
@@ -60,7 +59,7 @@ class DefaultProfanityHandlerTest {
         String word = "욕설이 들어간 문장입니다.";
 
         // when
-        ApiResponse response = ProfanityHandler.normalFilter(word);
+        ApiResponse response = profanityHandler.normalFilter(word);
 
         // then
         log.info("응답 : {}", response);
@@ -76,7 +75,7 @@ class DefaultProfanityHandlerTest {
         String word = "욕설이 들어간 문장입니다.";
 
         // when
-        ApiResponse response = ProfanityHandler.sanitizeProfanity(word);
+        ApiResponse response = profanityHandler.sanitizeProfanity(word);
 
         // then
         log.info("응답 : {}", response);
