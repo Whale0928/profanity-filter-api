097e24476c076a08e5ddf0c6e2a496e3f418d39d
hgkim <hgkim@openrd.com>
2025-01-30 20:54:33 +0900

    feat: email sender implements
    Signed-off-by: hgkim <hgkim@openrd.com>

--- profanity-api/src/main/java/app/application/EmailService.java
+++ profanity-api/src/main/java/app/application/EmailService.java
@@ -3,6 +3,7 @@ package app.application;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -11,27 +12,35 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.security.SecureRandom;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 @Slf4j
 @Service
 @EnableAsync
 @RequiredArgsConstructor
 public class EmailService {
+    private static final String EMAIL_KEY_PREFIX = "email:";  // 상수로 분리
+    private static final String EMAIL_TEMPLATE = "email-template";  // 상수로 분리
+
     private final JavaMailSender javaMailSender;
     private final SpringTemplateEngine templateEngine;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final Random random = new SecureRandom();
 
     @Async
     public void sendEmailNotice(String email) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         try {
+            String emailVerificationCode = createEmailVerificationCode(email);
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
             mimeMessageHelper.setTo(email);
             mimeMessageHelper.setSubject("인증 번호 안내"); // 메일 제목
-            mimeMessageHelper.setText(setContext(todayDate()), true);
+            mimeMessageHelper.setText(setContext(emailVerificationCode), true);
             mimeMessageHelper.setFrom("noreply@korea-profanity-filter.com", "한국어 비속어 검증 API [말조심하세욧]");
             javaMailSender.send(mimeMessage);
 
@@ -41,22 +50,30 @@ public class EmailService {
         }
     }
 
-    public String todayDate() {
-        ZonedDateTime todayDate = LocalDateTime.now(ZoneId.of("Asia/Seoul")).atZone(ZoneId.of("Asia/Seoul"));
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M월 d일");
-        return todayDate.format(formatter);
-    }
-
-    //thymeleaf를 통한 html 적용
-    public String setContext(String date) {
+    public String setContext(String code) {
         // 현재 시간으로부터 5분 후의 시간 계산
         LocalDateTime expirationTime = LocalDateTime.now(ZoneId.of("Asia/Seoul")).plusMinutes(5);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH시 mm분 ss초");
         String formattedTime = expirationTime.format(formatter);
 
         Context context = new Context();
-        context.setVariable("code", date);
+        context.setVariable("code", code);
         context.setVariable("expirationTime", formattedTime);
-        return templateEngine.process("todo", context);
+        return templateEngine.process(EMAIL_TEMPLATE, context);
+    }
+
+    public String createEmailVerificationCode(String email) {
+        String code = String.format("%06d", random.nextInt(1000000));
+        redisTemplate.opsForValue().set(EMAIL_KEY_PREFIX + email, code, Duration.ofMinutes(5));
+        return code;
+    }
+
+    public boolean verifyEmailCode(String email, String code) {
+        String storedCode = redisTemplate.opsForValue().get(EMAIL_KEY_PREFIX + email);
+        if (storedCode != null && storedCode.equals(code)) {
+            redisTemplate.delete(EMAIL_KEY_PREFIX + email);
+            return true;
+        }
+        return false;
     }
 }
--- profanity-api/src/main/java/app/presentation/ClientsController.java
+++ profanity-api/src/main/java/app/presentation/ClientsController.java
@@ -9,6 +9,7 @@ import app.core.data.response.constant.StatusCode;
 import app.domain.client.ClientMetadata;
 import app.dto.request.ClientRegistCommand;
 import app.dto.request.ClientRegistRequest;
+import app.dto.request.MailPayloadRequest;
 import app.dto.response.ClientsRegistResponse;
 import app.security.SecurityContextUtil;
 import jakarta.validation.Valid;
@@ -17,10 +18,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -41,19 +45,32 @@ public class ClientsController {
         return ApiResponse.ok(read);
     }
 
-    @GetMapping("/send-email")
-    public ResponseEntity<?> sendEmail(@RequestParam("email") String email) {
-        emailService.sendEmailNotice(email);
-        return ApiResponse.ok("send email");
-    }
-
     @PostMapping("/register")
     public ResponseEntity<?> registerClient(
             @RequestBody @Valid ClientRegistRequest request
     ) {
         final ClientRegistCommand command = request.toCommand();
-        // todo: 이메일 인증 구현
         ClientsRegistResponse response = clientsCommandService.registerNewClient(command);
         return ApiResponse.ok(response);
     }
+
+    @GetMapping("/send-email")
+    public ResponseEntity<?> sendEmail(@RequestParam("email") String email) {
+        //todo: 해당 사용자로 가입된 유저가 있는지 확인
+        emailService.sendEmailNotice(email);
+        return ApiResponse.ok("send email");
+    }
+
+    @PutMapping("/send-email")
+    public ResponseEntity<?> validEmail(@Valid @RequestBody MailPayloadRequest request) {
+        boolean verified = emailService.verifyEmailCode(request.email(), request.code());
+        if (Boolean.TRUE.equals(verified)) {
+            String apikey = clientReader.getApiKeyByEmail(request.email());
+            Map<String, String> response = Map.of("apikey", apikey);
+            return ApiResponse.ok(response);
+        } else {
+            return ApiResponse.error(Status.of(StatusCode.BAD_REQUEST, "이메일 인증 코드가 올바르지 않습니다."));
+        }
+    }
+
 }
