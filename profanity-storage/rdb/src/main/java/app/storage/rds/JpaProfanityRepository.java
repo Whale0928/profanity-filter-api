a196510bcb62b7d7de81ceab9cbc28b6df484589
hgkim <hgkim@openrd.com>
2025-02-16 00:32:36 +0900

    feat: add countAll method to ProfanityRepository and implement in JpaProfanityRepository
    Signed-off-by: hgkim <hgkim@openrd.com>

--- profanity-storage/rdb/src/main/java/app/storage/rds/JpaProfanityRepository.java
+++ profanity-storage/rdb/src/main/java/app/storage/rds/JpaProfanityRepository.java
@@ -3,6 +3,11 @@ package app.storage.rds;
 import app.domain.profanity.ProfanityRepository;
 import app.domain.profanity.ProfanityWord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JpaProfanityRepository extends ProfanityRepository, JpaRepository<ProfanityWord, Long> {
+
+    @Override
+    @Query("SELECT COUNT(p) FROM profanity_word p")
+    long countAll();
 }
