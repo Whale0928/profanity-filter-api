d8e5988fa54da6672cef1ccfd0e70b9c3441f855
hgkim <hgkim@openrd.com>
2025-02-16 00:32:18 +0900

    feat: add scheduling configuration with custom thread pool
    Signed-off-by: hgkim <hgkim@openrd.com>

--- profanity-domain/src/main/java/app/config/SchedulerConfig.java
+++ profanity-domain/src/main/java/app/config/SchedulerConfig.java
@@ -1,15 +1,23 @@
 package app.config;
 
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import javax.sql.DataSource;
+
 @Slf4j
 @Configuration
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30M")
 public class SchedulerConfig implements SchedulingConfigurer {
 
     @Override
@@ -29,5 +37,15 @@ public class SchedulerConfig implements SchedulingConfigurer {
 
         log.info("스케줄러가 초기화 되었습니다. [스레드 풀: {}개]", POOL_SIZE);
         taskRegistrar.setTaskScheduler(scheduler);
+    }
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime()
+                        .build()
+        );
     }
 }
